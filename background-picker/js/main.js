e5fd1aa8455844a5481876e0af59e2f6d9ad9c69
Franklin <frankkhuzy@gmail.com>
2023-08-01 06:53:40 -0700

    not so good calculator

--- background-picker/js/main.js
+++ background-picker/js/main.js
@@ -1,7 +1,7 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
-document.getElementById('red').onclick = partyRed
+document.getElementById('yellow').onclick = partyYellow
 
 function partyPurple() {
   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
@@ -18,7 +18,7 @@ function partyBlue() {
   document.querySelector('body').style.color = 'white'
 }
 
-function partyRed() {
-  document.querySelector('body').style.backgroundColorcolor = "rgb(235, 6, 6);"
+function partyYellow() {
+  document.querySelector('body').style.backgroundColorcolor = "yellow ;"
   document.querySelector('body').style.color ='white'
 }
